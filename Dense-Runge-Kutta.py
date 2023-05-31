42ff11a437464d4f9b38e38779277627b3740d9b
Diana Nitzschke <diddy-nit@web.de>
2023-05-31 12:02:42 +0200

    added Dense-Runge-Kutta.py

---
+++ Dense-Runge-Kutta.py
@@ -0,0 +1,7 @@
+"""
+The code below was written by @author: Diana Nitzschke and
+is an implementation of dense Runge-Kutta methods for the Dormand–Prince
+embedded Runge-Kutta method of order 4 and 5.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
