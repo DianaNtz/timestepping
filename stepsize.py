5fb9c00a5ba147b5dda2598f94f206b3123356eb
Diana Nitzschke <diddy-nit@web.de>
2023-06-01 14:54:33 +0200

    added initial data

--- stepsize.py
+++ stepsize.py
@@ -4,3 +4,22 @@ is an implementation of the embedded Dormand-Prince Runge-Kutta Method with adap
 """
 import numpy as np
 import matplotlib.pyplot as plt
+#some initial values
+t0=0
+tfinal=10
+steps=2**5
+dt=(tfinal-t0)/(steps)
+u0=0.0000001
+a=6
+#error tolerance
+tol=10**(-8)
+#differential equation function f
+def f(t,u):
+    return -(t-a)*u
+#setting up arrays
+t=np.empty(1, dtype='double')
+t[0]=t0
+tn=t0
+u=np.empty(1, dtype='double')
+u[0]=u0
+un=u0
