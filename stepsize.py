5126c3047fa2875249c8ecdaa20cae49654d29a7
Diana Nitzschke <diddy-nit@web.de>
2023-06-01 14:52:15 +0200

    created stepsize.py

---
+++ stepsize.py
@@ -0,0 +1,6 @@
+"""
+The code below was written by @author: Diana Nitzschke (https://github.com/DianaNtz) and
+is an implementation of the embedded Dormand-Prince Runge-Kutta Method with adaptive step size control
+"""
+import numpy as np
+import matplotlib.pyplot as plt
